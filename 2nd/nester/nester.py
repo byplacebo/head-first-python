0cceea208a6396309ce1ad01b29aa8d88b17e2ce
TylerLim <placebo2me@gmail.com>
2015-08-05 23:01:32 +0900

    complete all as chpater 2

--- 2nd/nester/nester.py
+++ 2nd/nester/nester.py
@@ -2,7 +2,7 @@
 which prints lists that may or may not include nested lists."""
 
 
-def print_lol(the_list, level):
+def print_lol(the_list, indent=False, level=0):
     """This function takes one positional argument called "The_list", which
     is any Python list (of - possibly - nested lists). Each data item in the
     provided list is (recursively) printed to the screen on it's own line.
@@ -10,10 +10,11 @@ def print_lol(the_list, level):
 
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, level + 1)
+            print_lol(each_item, indent, level + 1)
         else:
-            for tab_stpo in range(level):
-                print("\t", end='')
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
             print(each_item)
 
 
