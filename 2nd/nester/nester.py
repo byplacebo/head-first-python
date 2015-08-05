77e01e05a6ba32eca5c334619e1733814409f510
TylerLim <placebo2me@gmail.com>
2015-08-05 22:49:41 +0900

    complete page95 on chapter 2

--- 2nd/nester/nester.py
+++ 2nd/nester/nester.py
@@ -2,7 +2,7 @@
 which prints lists that may or may not include nested lists."""
 
 
-def print_lol(the_list):
+def print_lol(the_list, level):
     """This function takes one positional argument called "The_list", which
     is any Python list (of - possibly - nested lists). Each data item in the
     provided list is (recursively) printed to the screen on it's own line.
@@ -10,16 +10,14 @@ def print_lol(the_list):
 
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item)
+            print_lol(each_item, level + 1)
         else:
+            for tab_stpo in range(level):
+                print("\t", end='')
             print(each_item)
 
 
 """
 Sample Data.
-moives = ["The Holy Grail, 1975, "Terry Jones & Terry Gilliam", 91,
-    ["Graham Chapman",
-        ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle & Terry Jones"]]]
-
-
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle & Terry Jones"]]]
 """
